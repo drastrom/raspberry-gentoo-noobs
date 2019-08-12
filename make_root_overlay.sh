e92aa46420c745f660dae0c56bc70ff2dc0f7a55
Jeremy Drake <github@jdrake.com>
2019-08-12 10:00:58 -0700

    Use more specific wildcard for wifi firmware.
    New firmware-nonfree includes more broadcom firmware than just raspberry
    pi.

--- make_root_overlay.sh
+++ make_root_overlay.sh
@@ -10,7 +10,7 @@ popd
 
 mkdir -p root_overlay/lib/{modules,firmware/brcm}
 mkdir -p root_overlay/usr/bin
-cp -f gitmodules/firmware-nonfree/brcm/brcmfmac434* root_overlay/lib/firmware/brcm
+cp -f gitmodules/firmware-nonfree/brcm/brcmfmac434{30,55}* root_overlay/lib/firmware/brcm
 cp -f gitmodules/bluez-firmware/broadcom/BCM434* root_overlay/lib/firmware
 cp -Rf gitmodules/firmware/modules/*-v7+/ root_overlay/lib/modules/
 cp -f gitmodules/pi-bluetooth/usr/bin/* root_overlay/usr/bin/
